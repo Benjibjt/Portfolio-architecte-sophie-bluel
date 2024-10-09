8a548b7ac30c35c4f3a5d9c4ca20b82559397b25
John Doe <johndoe@example.com>
2024-10-09 13:00:13 +0200

    Ajout de la fonction + de la boucle en vue d'afficher le portfolio avant connexion à la BDD

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,15 +1,27 @@
-const gallery = document.getElementById('gallery');
-const figure = document.createElement('figure');
-const img = document.createElement('img');
-const figcaption = document.createElement('figcaption');
 
-img.src = "Test image"; //à la place on aura un lien vers BDD pour récupérer les images
-img.alt = "Test titre"; //le même texte que le figcaption
-figcaption.textContent = "Test titre";
+//Fonction en vue d'afficher les éléments du portfolio
+function displayPortfolio(projects) {
+    const gallery = document.getElementById('gallery');
 
-gallery.appendChild(figure);
-figure.appendChild(img);
-figure.appendChild(figcaption);
+    //Boucle pour parcourir les projets - projects 
+    for (let i = 0; i < projects.length; i++) {
+        const figure = document.createElement('figure');
+        const img = document.createElement('img');
+        const figcaption = document.createElement('figcaption');
+
+        // En prévision de la récupération des données depuis la BDD
+        img.src = "Test image"; // à la place on aura un lien vers BDD pour récupérer les images. Du genre "projects[i].imageUrl"
+        img.alt = "Test titre"; // le même que le figcaption. Idem on va récupérer ensuite les titres en BDD. Ex: "projects[i].title"
+        figcaption.textContent = "Test titre"; // idem 
+
+        // Assemble les éléments dans le DOM
+        gallery.appendChild(figure);
+        figure.appendChild(img);
+        figure.appendChild(figcaption);
+
+    }
+    
+}
 
 
 
